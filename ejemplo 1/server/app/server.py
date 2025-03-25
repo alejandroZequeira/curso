9035762bad3492f71fb96df8739292efbbf19874
alejandroZequeira <alejandrozequeirarosa@gmail.com>
2025-03-25 14:47:10 -0600

    update example

--- ejemplo 1/server/app/server.py
+++ ejemplo 1/server/app/server.py
@@ -2,12 +2,6 @@ import socket
 import threading
 import os
 
-
-def enviar_archivo(nombre_archivo, cliente):
-    with open("data/"+nombre_archivo, 'rb') as archivo:
-        datos = archivo.read()
-        cliente.sendall(datos)
-
 def server(port):
     host="0.0.0.0"
     with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as servidor:
@@ -19,11 +13,6 @@ def server(port):
             nombre_archivo = cliente.recv(1024).decode()
             print(f"conexion desde:{direccion}")
             print("Recibiendo archivo:", nombre_archivo)
-            if not os.path.exists("data"):
-                os.makedirs("data")
-            with open("data/"+nombre_archivo, 'wb') as archivo:
-                datos = cliente.recv(1024)
-                archivo.write(datos)
             cliente.sendall("archivo recibido")
             cliente.close()
 
